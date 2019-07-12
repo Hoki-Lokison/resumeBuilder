e466961d37f045206fd601d78400ba51c03a0a04
Malia <51797731+Hoki-Lokison@users.noreply.github.com>
2019-07-12 12:29:30 -0600

    working on forms

--- code.js
+++ code.js
@@ -1,22 +1,28 @@
+//var url = "http://localhost:3000";
+var url = "https://createresume.herokuapp.com";
 
 
 var app= new Vue ({
-  el: "#app1",
+    el: "#app1",
 
-  data: {
+    data: {
+      menu:false,
+      modal: false,
+      page: "form",
+      color: "",
+
+        educationlist:[],
+        workexplist:[],
+        accomplishmentlist: [],
+        extracurricularlist:[],
+        languageslist:[],
+        programslist:[],
+        softskillslist:[],
+        awardslist:[],
+        statementlist:[],
 
 
-      educationlist:[],
-      workexplist:[],
-      accomplishmentlist: [],
-      extracurricularlist:[],
-      languageslist:[],
-      programslist:[],
-      softskillslist:[],
-      awardslist:[],
-      statementlist:[],
-
-      personalinfoEdit:
+        personalinfoEdit:
         {
             first_name:"",
             last_name:"",
@@ -32,63 +38,77 @@ var app= new Vue ({
             linkedin: "",
         },
 
-      statementEdit: {
-        statement: "",
-      },
+        statementEdit: {
+          statement: "",
+        },
 
-      workexpEdit: {
-          work_company: "",
-          work_title: "",
-          work_startdate: new Date().toISOString().substr(0, 10),
-          work_enddate: new Date().toISOString().substr(0, 10),
-          work_description: "",
-          start_menu: false,
-          end_menu: false,
+        workexpEdit: {
+            work_company: "",
+            work_title: "",
+            work_startdate: new Date().toISOString().substr(0, 10),
+            work_enddate: new Date().toISOString().substr(0, 10),
+            work_description: "",
+            start_menu: false,
+            end_menu: false,
+            position: 0, // for each v-card in a template, do for loop through all categories and see which has the desired position
 
-      },
+        },
 
-      educationEdit: {
-        college: "",
-        degree: "",
-        gradyear: "",
-        start_menu: false,
-        end_menu: false,
-      },
+        educationEdit: {
+          college: "",
+          degree: "",
+          gradyear: new Date().toISOString().substr(0, 10),
+          menu: false
+        },
 
-      accomplishmentEdit: {
-        title: "",
-        description: "",
-      },
-      extracurricularEdit: {
-        title: "",
-        description: "",
-        date: "",
-      },
-      languagesEdit: {
-        title: "",
-        proficiency:  "",
-      },
-      programsEdit: {
-        title: "",
-        proficiency:  "",
-      },
-      softskillsEdit: {
-        title: "",
-      },
-      awardsEdit: {
-        title: "",
-        receivedfrom:  "",
-        date: "",
-        description: "",
-      },
-      proficiencylist: [
-        "Beginner",
-        "Intermediate",
-        "Proficient",
-        "Advanced",
-        "Expert"
-      ],
-      page: "form",
+        accomplishmentEdit: {
+          title: "",
+          description: "",
+        },
+        extracurricularEdit: {
+          title: "",
+          description: "",
+          date: "",
+        },
+        languagesEdit: {
+          title: "",
+          proficiency:  "",
+        },
+        programsEdit: {
+          title: "",
+          proficiency:  "",
+        },
+        softskillsEdit: {
+          title: "",
+        },
+        awardsEdit: {
+          title: "",
+          receivedfrom:  "",
+          date: new Date().toISOString().substr(0, 10),
+          description: "",
+          menu:false
+        },
+        proficiencylist: [
+          "Beginner",
+          "Intermediate",
+          "Proficient",
+          "Advanced",
+          "Expert"
+        ],
+        colors: [
+            "orange",
+            "black",
+            "red",
+            "blue",
+            "green"
+        ],
+        selected_color_main: "rgb(84, 174, 219)",
+        selected_color_accent: "",
+        pickingColorMain: false,
+        pickingColor: false,
+        color_brightness: 6,
+        accent: 0,
+
       template: "malia",
       templateLabel: "Choose a Template",
       templates: [
@@ -109,19 +129,6 @@ var app= new Vue ({
           name: "Template 4"
         },
       ],
-      colors: [
-          "orange",
-          "black",
-          "red",
-          "blue",
-          "green"
-      ],
-      selected_color_main: "rgb(84, 174, 219)", //changed
-      selected_color_accent: "",
-      pickingColorMain: false, //changed
-      pickingColor: false,
-      color_brightness: 6,
-      accent: 0,
 
       statementdisplay: [],
       workexpdisplay: [],
@@ -133,185 +140,283 @@ var app= new Vue ({
       softskillsdisplay: [],
       awardsdisplay: [],
 
+      add_remove: "",
 
+      workexpposition: "",
+      position1: {},
 
-  },
+      //changed
+      emailRules: [ //changed
+        v => !!v || 'E-mail is required',
+        v => /.+@.+/.test(v) || 'E-mail must be valid'
+      ],
+      phoneRules: [ //changed
+        v => !!v || 'Phone Number is required',
 
-  created: function () {
-    // removed both new wheels
-    addEventListener("click", function () { //changed
-      app.selected_color_main = document.getElementById("colorMain").style.backgroundColor;
-      console.log("Main Color: ", app.selected_color_main);
-      app.selected_color_accent = document.getElementById("colorAccent").style.backgroundColor;
-      console.log("Accent Color: ", app.selected_color_accent);
-    });
-  },
+      ],
 
-  methods: {
-    addStatement: function(){
-      this.statementlist.push(this.statementEdit)
-
-      this.statementEdit= {
-        statement: "",
-      }
     },
-    addWork: function(){
-      this.workexplist.push(this.workexpEdit)
-      this.workexpEdit={
-        work1company: "",
-        work1title: "",
-        work1startdate: "",
-        work1enddate: "",
-        work1description: "",
+    created: function () {
 
-      }
+      this.getData("statement")
+      this.getData("workexp")
+      this.getData("education")
+      this.getData("accomplishment")
+      this.getData("extracurricular")
+      this.getData("language")
+      this.getData("program")
+      this.getData("softskill")
+      this.getData("award")
+
+      addEventListener("click", function () {
+        app.selected_color_main = document.getElementById("colorMain").style.backgroundColor;
+        console.log("Main Color: ", app.selected_color_main);
+        app.selected_color_accent = document.getElementById("colorAccent").style.backgroundColor;
+        console.log("Accent Color: ", app.selected_color_accent);
+      });
+
     },
+
+
+    methods: {
+
+      changePhone: function () { //changed
+        var newNum = this.personalinfoEdit.phone.match(/(\d{3})(\d{3})(\d{4})/);
+        this.personalinfoEdit.phone = "(" + newNum[1] + ") " + newNum[2] + "-" + newNum[3];
+      },
+
+      addStatement: function(){
+        this.statementlist.push(this.statementEdit)
+
+        this.statementEdit= {
+          statement: "",
+        }
+      },
+      addWork: function(){
+        this.workexplist.push(this.workexpEdit)
+        this.workexpEdit={
+          work1company: "",
+          work1title: "",
+          work1startdate: "",
+          work1enddate: "",
+          work1description: "",
+
+        }
+      },
       addEducation: function(){
-        this.educationlist.push(this.educationEdit)
+          this.educationlist.push(this.educationEdit)
 
-        this.educationEdit= {
-          college: "",
-          degree: "",
-          gradyear: "",
-          menu: false
+          this.educationEdit= {
+            college: "",
+            degree: "",
+            gradyear: "",
+            menu: false
+          }
+
+      },
+      addAccomplishment: function(){
+        this.accomplishmentlist.push(this.accomplishmentEdit)
+
+        this.accomplishmentEdit= {
+          title: "",
+          description: "",
         }
 
-    },
+      },
+      addLanguage: function(){
+        this.languageslist.push(this.languagesEdit)
 
-    addAccomplishment: function(){
-      this.accomplishmentlist.push(this.accomplishmentEdit)
+        this.languagesEdit= {
+          title: "",
+          proficiency:  "",
+        }
 
-      this.accomplishmentEdit= {
-        title: "",
-        description: "",
-      }
 
-    },
+      },
+      addSkill: function(){
+        this.programslist.push(this.programsEdit)
 
-    addLanguage: function(){
-      this.languageslist.push(this.languagesEdit)
+        this.programsEdit= {
+          title: "",
+          proficiency:  "",
+        }
 
-      this.languagesEdit= {
-        title: "",
-        proficiency:  "",
-      }
-    },
 
-    addSkill: function(){
-      this.softskillslist.push(this.softskillsEdit)
+      },
+      addAward: function(){
+        this.awardslist.push(this.awardsEdit)
 
-      this.softskillsEdit= {
-        title: "",
-        proficiency:  "",
-      }
-    },
+        this.awardsEdit= {
+          title: "",
+          receivedfrom:  "",
+          date: "",
+          description: "",
+        }
+      },
+      addExtracurricular: function(){
+        this.extracurricularlist.push(this.extracurricularEdit)
 
-    addExtracurricular: function(){
-      this.extracurricularlist.push(this.extracurricularEdit)
+        this.extracurricularEdit= {
+          title: "",
+          proficiency:  "",
+        }
+      },
 
-      this.extracurricularEdit= {
-        title: "",
-        proficiency:  "",
-      }
-    },
+      includeStatement: function(exp) {
+        this.statementdisplay.push(exp);
+        this.add_remove = "remove";
+        return true;
+      },
+      includeWork: function(exp) {
+        this.workexpdisplay.push(exp);
+        this.add_remove = "remove";
+        return true;
+      },
+      includeEducation: function(exp) {
+        this.educationdisplay.push(exp);
+        this.add_remove = "remove";
+        return true;
+      },
+      includeAccomplishment: function(exp) {
+        this.accomplishmentdisplay.push(exp);
+        this.add_remove = "remove";
+        return true;
+      },
+      includeAward: function(exp) {
+        this.awardsdisplay.push(exp);
+        this.add_remove = "remove";
+        return true;
+      },
+      includeProgram: function(exp) {
+        this.programsdisplay.push(exp);
+        this.add_remove = "remove";
+        return true;
+      },
+      includeSkill: function(exp) {
+        this.softskillsdisplay.push(exp);
+        this.add_remove = "remove";
+        return true;
+      },
+      includeExtracurricular: function(exp) {
+        this.extracurriculardisplay.push(exp);
+        this.add_remove = "remove";
+        return true;
+      },
+      includeLanguage: function(exp) {
+        this.languagesdisplay.push(exp);
+        this.add_remove = "remove";
+        return true;
+      },
 
-    addAward: function(){
-      this.awardslist.push(this.awardsEdit)
 
-      this.awardsEdit= {
-        title: "",
-        receivedfrom:  "",
-        date: "",
-        description: "",
-      }
-    },
+      priorityOne: function (exp) {
+        this.priority1.push(exp);
+      },
 
-    includeWork: function(exp) {
-      this.workexpdisplay.push(exp);
-      return true;
-    },
-    includeEducation: function(exp) {
-      this.workexpdisplay.push(exp);
-      return true;
-    },
-    includeAccomplishment: function(exp) {
-      this.workexpdisplay.push(exp);
-      return true;
-    },
-    includeAward: function(exp) {
-      this.workexpdisplay.push(exp);
-      return true;
-    },
-    includeProgram: function(exp) {
-      this.workexpdisplay.push(exp);
-      return true;
-    },
-    includeSkill: function(exp) {
-      this.workexpdisplay.push(exp);
-      return true;
-    },
-    includeExtracurricular: function(exp) {
-      this.workexpdisplay.push(exp);
-      return true;
-    },
-    includeLanguage: function(exp) {
-      this.workexpdisplay.push(exp);
-      return true;
-    },
-    includeStatement: function(exp) {
-      this.workexpdisplay.push(exp);
-      return true;
-    },
+      newKellyColorPickerMain: function () {
+        if (this.pickingColorMain == false) {
+          new KellyColorPicker({
+            place : 'color-picker-main',
+            size : 150,
+            input : 'colorMain',
+            method: 'triangle',
+            input_format: "rgba",
+            alpha_slider: true,
+            display: 'block',
+          });
+          this.pickingColorMain = true;
+        } else if (this.pickingColorMain == true) {
+          this.pickingColorMain = false;
+        };
+      },
+      newKellyColorPickerAccent: function () {
+        if (this.pickingColor == false) {
+          new KellyColorPicker({
+            place : 'color-picker-accent',
+            size : 150,
+            input : 'colorAccent',
+            method: 'triangle',
+            input_format: "rgba",
+            alpha_slider: true,
+            display: 'block',
+          });
+          this.pickingColor = true;
+        } else if (this.pickingColor == true) {
+          this.pickingColor = false;
+        };
+      },
 
-    newKellyColorPickerMain: function () { //changed
-      if (this.pickingColorMain == false) {
-        new KellyColorPicker({
-          place : 'color-picker-main',
-          size : 150,
-          input : 'colorMain',
-          method: 'triangle',
-          input_format: "rgba",
-          alpha_slider: true,
-          display: 'block',
+      pdfSave: function () {
+        var doc = new jsPDF();
+        var specialElementHandlers = {
+            '#editor': function (element, renderer) {
+                return true;
+            }
+        };
+        doc.fromHTML($('#resume').html(), 15, 15, {
+          'width': 170,
+          'elementHandlers': specialElementHandlers
         });
-        this.pickingColorMain = true;
-      } else if (this.pickingColorMain == true) {
-        this.pickingColorMain = false;
-      };
+        doc.save(this.personalinfoEdit.first_name+'_Resume.pdf');
+      },
+
+
+      getData: function(want) {
+        fetch(`${url}/${want}`).then(function (response) {
+          response.json().then(function (data) {
+
+            if(want=="statement"){
+              app.statementlist = data.statementlist
+            }
+            if(want=="workexp"){
+              app.workexplist = data.workexplist
+            }
+            if(want=="education"){
+              app.educationlist = data.educationlist
+            }
+            if(want=="accomplishment"){
+              app.accomplishmentlist = data.accomplishmentlist
+            }
+            if(want=="extracurricular"){
+              app.extracurricularlist = data.extracurricularlist
+            }
+            if(want=="language"){
+              app.languageslist = data.languagelist
+            }
+            if(want=="program"){
+              app.programslist = data.programlist
+            }
+            if(want=="softskill"){
+              app.softskillslist = data.softskilllist
+            }
+            if(want=="award"){
+              app.awardslist = data.awardlist
+            }
+
+            });
+          });
+        },
+
     },
-    newKellyColorPickerAccent: function () { //changed
-      if (this.pickingColor == false) {
-        new KellyColorPicker({
-          place : 'color-picker-accent',
-          size : 150,
-          input : 'colorAccent',
-          method: 'triangle',
-          input_format: "rgba",
-          alpha_slider: true,
-          display: 'block',
-        });
-        this.pickingColor = true;
-      } else if (this.pickingColor == true) {
-        this.pickingColor = false;
-      };
-    },
-    pdfSave: function () { //changed
-      var doc = new jsPDF();
-      var specialElementHandlers = {
-          '#editor': function (element, renderer) {
-              return true;
+
+    computed: {
+        /* changed; take this whole thing out:
+        computedColorClass: function () {
+          return {
+            "lighten-5": this.color_brightness == 1,
+            "lighten-4": this.color_brightness == 2,
+            "lighten-3": this.color_brightness == 3,
+            "lighten-2": this.color_brightness == 4,
+            "lighten-1": this.color_brightness == 5,
+            "darken-1": this.color_brightness == 7,
+            "darken-2": this.color_brightness == 8,
+            "darken-3": this.color_brightness == 9,
+            "darken-4": this.color_brightness == 10,
+            "accent-1": this.accent == 11,
+            "accent-2": this.accent == 12,
+            "accent-3": this.accent == 13,
+            "accent-4": this.accent == 14,
           }
-      };
-      doc.fromHTML($('#resume').html(), 15, 15, {
-        'width': 170,
-        'elementHandlers': specialElementHandlers
-      });
-      doc.save(this.personalinfoEdit.first_name+'_Resume.pdf');
-    },
-
-  },
-
-  computed: {
-
-    },
+        }, */
+      },
 })
